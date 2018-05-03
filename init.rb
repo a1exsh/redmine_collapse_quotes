e0132bcd6547e90482ab24e5f98a41ed05c75907
gc-olivierho <olivier.houdas@geoconcept.com>
2018-05-03 11:29:25 +0200

    version number update

--- init.rb
+++ init.rb
@@ -24,7 +24,7 @@ Redmine::Plugin.register :redmine_collapse_quotes do
   name 'Redmine Collapse Quotes plugin'
   author 'Alex Shulgin <ash@commandprompt.com>'
   description 'Redmine plugin to collapse quotes in issue history'
-  version '0.0.1'
+  version '0.0.2'
 #  url 'https://github.com/commandprompt/redmine_collapse_quotes/'
 #  author_url 'http://example.com/about'
 
